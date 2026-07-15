07f86a825e1ccd70cc7a87008ca6375578b398f7
hskwon1133 <81279379+hskwon1133@users.noreply.github.com>
2026-07-15 16:47:33 +0900

    Update sql_version.sql

--- sql_version.sql
+++ sql_version.sql
@@ -3,4 +3,4 @@ select a.skuseq
       ,b.mngcate
       ,b.cate3||'>'||b.cate4||'>'||b.cate5 as kan_cate
 from bimart.dwd_sku a
-join bimart.ddd_catetgory_management b  on a.mngcate = b.mngcate
+join bimart.ddd_category_management_hier b  on a.mngcate = b.mngcate
